6cfd559bff85bbbdbf79b83dcec76baaecdce7b2
Jun Oi <joi@joi-ltm2.internal.salesforce.com>
2019-09-25 10:12:32 +0900

    add logo

--- force-app/main/default/lwc/myCalendar/myCalendar.js
+++ force-app/main/default/lwc/myCalendar/myCalendar.js
@@ -1,11 +1,14 @@
 import { LightningElement, wire, track } from 'lwc';
 import getWelcomeMessage from '@salesforce/apex/MyCalendarCtrl.getWelcomeMessage';
+import astro_image from '@salesforce/resourceUrl/logo';
 
 export default class MyCalendar extends LightningElement {
     @track greetingMessage;
     @track warekiDate;
     @track error;
     
+    astro = astro_image;
+
     @wire(getWelcomeMessage)
     wiredWelcomeMessage({error, data}) {
         if (data) {
